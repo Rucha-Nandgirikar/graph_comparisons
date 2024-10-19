c3f89c9d993105530fd868b65b3d49b299fc8b64
BradJKim <bradjkim03@gmail.com>
2024-10-18 23:13:47 -0700

    created helper display functions for organization

--- src/views/js/index.js
+++ src/views/js/index.js
@@ -4,11 +4,11 @@ import {
 } from "./mainstudy.js"
 
 import { 
-  getUserId,
   recordInteraction,
   recordPrestudyResponse,
   prestudyQuestions,
   assignUserId,
+  prestudyQuestions,
 } from "./prestudy.js"
 
 import {
@@ -19,7 +19,8 @@ import {
 } from "./charts.js" 
 
 document.addEventListener('DOMContentLoaded', () => {
-  const beginButton = document.getElementById('begin');
+  // on-launch elements
+  const beginStudyButtonWrapper = document.getElementById('begin');
   const beginStudyButton = document.getElementById('begin-study-button');
   const homeContent = document.getElementById('home-content');
   const claimUserIdButton = document.getElementById('claim-user-id');
@@ -27,6 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const prestudyNotif = document.getElementById('prestudy-notif');
 
   //prestudy elements
+  const userQuestions = prestudyQuestions;
   const beginPrestudyButton = document.getElementById("begin-prestudy-button");
   const prestudyContent = document.getElementById("prestudy");
   const prestudyQuestionElement = document.getElementById("prestudy-question");
@@ -62,11 +64,15 @@ document.addEventListener('DOMContentLoaded', () => {
   // Post study elements
   const postStudyCongrats = document.getElementById('congrats-cat')
   
+
+  /** 
+   * EVENT HANDLERS
+   */  
+
   // Step 1: Show home content on "BEGIN" button click
   beginStudyButton.addEventListener('click', () => {
-    // here db interaction is done for persisting the user id
-    homeContent.style.display = 'flex'; 
-    beginButton.style.display = 'none';
+    hideBeginStudyScreen();
+    showHomeScreen();
   });
 
   // Step 2: Show user ID and prestudy info when "CLAIM YOUR USER ID" is clicked
@@ -74,23 +80,23 @@ document.addEventListener('DOMContentLoaded', () => {
     const data = await assignUserId();
     userId = data.userId;
 
-    claimUserIdButton.style.display = "none";
-    showUserId.textContent = `User ID: ${data.userId}`; 
-    prestudyNotif.style.display = 'block'; 
-    beginPrestudyButton.style.display = 'block';
-
+    showUserIdElements()
   });
 
   // Step 3: Switch to prestudy questions when "BEGIN PRESTUDY" is clicked
   beginPrestudyButton.addEventListener('click', () => {
-    homeContent.style.display = 'none';  // Hide home content
+    hideHomeScreen();
+    
+    showPrestudyScreen();
+    displayNextPrestudyQuestion();
 
-    // dev reasons
-    //displayPrestudyQuestions(prestudyQuestions);
-
-    beginMainStudy();
+    // uncomment for main study
+    /* 
+      showMainStudyScreen();
+      displayNextQuestion();
+    */
+   
   });
-
 
   // Step 4: Handle prestudy submission and show the next button
   prestudySubmitButton.addEventListener("click", async () => {
@@ -105,38 +111,47 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     recordPrestudyResponse(userId, currentQuestion, currentAnswer);
+    recordInteraction(userId, "Submit", false, true, currentQuestionId, currentQuestion, currentAnswer);
     inputElement.value = "";
   
-    displayPrestudyQuestions(prestudyQuestions);
-    recordInteraction(userId, "Submit", false, true, currentQuestionId, currentQuestion, currentAnswer);
+    if (currentPrestudyQuestionIndex < 6) {
+      displayNextPrestudyQuestion();
+    }
+    else {
+      hidePrestudyScreen();
+      //showCalibrationScreen();
+      showBeginMainStudyScreen();
+    }
 
   });
 
-  // Step 5: Switch to calibration screen when "NEXT" is clicked
+  // Step 4.5 (OPTIONAL): Switch to calibration screen when "NEXT" is clicked
   startCalibrationButton.addEventListener('click', () => {
-    calibrationScreen.style.display = 'block'; // Show calibration screen
+    hideCalibrationScreen();
+    triggerCalibration();
 
-    // For example, after calibration ends, you can show the button for the main study
     setTimeout(() => {
-      calibrationScreen.style.display = 'none';  // Hide calibration screen
-      beginMainStudyElement.style.display = 'flex'; // Show "BEGIN MAIN STUDY" button
+      showBeginMainStudyScreen();
     }, 3000); // Simulate a 3-second calibration screen
   });
 
-  // Step 6: Begin Main Study
+  // Step 5: Begin Main Study
   beginMainStudyButton.addEventListener("click", () => {
     recordInteraction(userId, "Begin Main Study", false, false, currentQuestionId, currentQuestion, currentAnswer);
-    beginMainStudy(); 
+    hideBeginMainStudyScreen();
+    
+    // Start Study
+    loadStudyQuestions(); 
+    showMainStudyScreen();
+    displayNextQuestion();
   });
   
   // Reload Button for chart
   reloadButton.addEventListener('click', () => {
-    clearGraphs()
-
-    const currentRow = data2DArray[currentQuestionIndex];
-    displayGraph(currentRow[7])
+    reloadGraph();
   });
 
+  // Submit user response and display next question
   submitButton.addEventListener("click", () => {
     checkAnswer(userId, currentQuestionId, currentQuestion, currentCorrectAnswer, currentAnswer);
     recordInteraction(userId, "Submit", true, false, currentQuestionId, currentQuestion, currentAnswer);
@@ -151,144 +166,98 @@ document.addEventListener('DOMContentLoaded', () => {
       ).value;
     }
     
-    displayNextQuestion()
+    if (currentQuestionIndex < data2DArray.length) {
+      displayNextQuestion()
+    } else {
+      hideMainStudyScreen();
+      showPostStudyCongrats();
+    }
   });
 
 
-  /* FUNCTIONS */
+  /** 
+   * HELPER FUNCTIONS
+   */  
   
-  
+  function hideBeginStudyScreen() {
+    beginStudyButtonWrapper.style.display = 'none';
+  }
+
+  function showHomeScreen() {
+    homeContent.style.display = 'flex'; 
+  }
+
+  function showUserIdElements() {
+    claimUserIdButton.style.display = "none";
+    showUserId.textContent = `User ID: ${data.userId}`; 
+    prestudyNotif.style.display = 'block'; 
+    beginPrestudyButton.style.display = 'block';
+  }
+
+  function hideHomeScreen() {
+    homeContent.style.display = 'none';
+  }
+
+  function showCalibrationScreen(){
+    startCalibrationButton.style.display = "flex";
+  }
+
+  function hideCalibrationScreen(){
+    startCalibrationButton.style.display = "none";
+  }
+
+  function showPrestudyScreen() {
+    prestudyContent.style.display = "flex";
+    prestudySubmitButton.style.display = "block";
+  }
+
+  function hidePrestudyScreen() {
+    prestudyContent.style.display = 'none';
+  }
+
+  function showBeginMainStudyScreen() {
+    beginMainStudyElement.style.display = 'flex';
+  }
+
+  function hideBeginMainStudyScreen() {
+    beginMainStudyButton.style.display = "none";
+    beginMainStudyElement.style.display = 'none';
+  }
+
+  function showMainStudyScreen() {
+    mainStudy.style.display = "block";
+    submitButton.style.display = "block";
+  }
+
+  function hideMainStudyScreen() {
+    mainStudy.style.display = "none";
+    submitButton.style.display = "none";
+    optionsElement.innerHTML = "";
+    chartPlaceholder.innerHTML = "";
+  }
+
+  function showPostStudyCongrats() {
+    postStudyCongrats.style.display = "block";
+    questionElement.textContent = "Study complete. Thank you for participating!";
+  }
+
+  function triggerCalibration() {
+    startCalibrationButton.style.display = "block";
+    prestudyMsgElement.textContent =
+      "Prestudy completed! When you click NEXT, you will be shown a blank screen with a tiny plus sign at the center, please focus your eyes on it for 5 seconds. ";
+    prestudyQuestionElement.innerHTML = "";
+    prestudySubmitButton.style.display = "none";
+    currentPrestudyQuestionIndex = 0;
+    prestudyChart.innerHTML = "";
+    inputElement.style.display = "none";
+  }
+
   /**
    * Retrieve/store questions and begin Main Study
    */
-  async function beginMainStudy() {
+  async function loadStudyQuestions() { 
     const tableData = await getTableData();
-    storeQuestionsInArray(tableData);
- 
-    homeContent.style.display = "none"; // Hide the welcome message
-    mainStudy.style.display = "block"; // Show the study content
-    beginMainStudyButton.style.display = "none";
- 
-    displayNextQuestion(); //display next main study question
-  }
 
-  /**
-   *  Display next prestudy question
-   */
-  function displayPrestudyQuestions(questions) {
-    prestudyContent.style.display = "flex";
-    prestudySubmitButton.style.display = "block";
-    startCalibrationButton.style.display = "none";
-    
-    if (currentPrestudyQuestionIndex < 6) {
-      prestudyQuestionElement.innerHTML = currentQuestion.value =
-        questions[currentPrestudyQuestionIndex][0];
-      prestudyChart.innerHTML = "";
-    
-      var imageElement = document.createElement("img");
-      if (questions[currentPrestudyQuestionIndex][1] != null) {
-        imageElement.src =
-          "img/prestudy-img/" + questions[currentPrestudyQuestionIndex][1];
-        imageElement.alt = "prestudy Chart";
-        imageElement.style.width = "auto";
-        imageElement.style.height = "400px";
-        prestudyChart.appendChild(imageElement);
-      }
-
-      currentPrestudyQuestionIndex++;
-    } else {
-      // Survey is complete
-      startCalibrationButton.style.display = "block";
-      prestudyMsgElement.textContent =
-        "Prestudy completed! When you click NEXT, you will be shown a blank screen with a tiny plus sign at the center, please focus your eyes on it for 5 seconds. ";
-      prestudyQuestionElement.innerHTML = "";
-      prestudySubmitButton.style.display = "none";
-      currentPrestudyQuestionIndex = 0;
-      prestudyChart.innerHTML = "";
-      inputElement.style.display = "none";
-    }
-  }
-
-  /**
-   * Display next Question in question array
-   */
-  function displayNextQuestion() {
-    if (currentQuestionIndex < data2DArray.length) {
-      submitButton.style.display = "block";
-      
-      const currentRow = data2DArray[currentQuestionIndex];
-
-      // Assign value to the question text
-      questionElement.textContent = currentQuestion.value = `${currentQuestionIndex + 1}. ${currentRow[0]}`;
-      optionsElement.innerHTML = "";
-      chartPlaceholder.innerHTML = "";
-
-      currentCorrectAnswer = currentRow[4];
-
-      // Create iframe element using graphURL and URLParams
-      const graphURL = currentRow[1];
-      const iframeElement = document.createElement("iframe");
-      iframeElement.src = `${graphURL}`;
-      iframeElement.width = "100%";
-      iframeElement.height = "600px";
-      iframeElement.style.border = "none";
-      
-      const graphId = currentRow[7]
-
-      displayGraph(graphId)
-      
-      const options = currentRow[3]; 
-
-      options.forEach((option, index) => {
-        const label = document.createElement("label");
-        const input = document.createElement("input");
-        input.type = "radio";
-        input.name = "answer";
-        input.value = option;
-        label.appendChild(input);
-        label.appendChild(document.createTextNode(option));
-        optionsElement.appendChild(label);
-      });
-      
-      currentQuestionIndex++;
-    } else {
-      currentQuestionIndex = 0;
-      postStudyCongrats.style.display = "block";
-      questionElement.textContent = "Study complete. Thank you for participating!";
-      optionsElement.innerHTML = "";
-      chartPlaceholder.innerHTML = "";
-      submitButton.style.display = "none";
-    }
-  }
-
-  /**
-   * Displays appropriate graph based on graphId
-   */
-  function displayGraph(graphId) {
-    if(graphId === 1){
-      displayMyEquityGapsMajorGaps(chartPlaceholder);
-    } 
-    else if(graphId === 2){
-      displayStudentProgressUnits(chartPlaceholder);
-    }
-    else {
-      chartPlaceholder.appendChild(iframeElement);
-    }
-  }
-    
-  /**
-   * Erases current graphs
-   */
-  function clearGraphs() {
-    while (chartPlaceholder.firstChild) {
-      chartPlaceholder.removeChild(chartPlaceholder.lastChild);
-    }
-  }
-
-  /**
-   * Stores questions in tableData array
-   */
-  function storeQuestionsInArray(tableData) {    
     for (const entry of tableData) {
       const questionText = entry.question_text;
       const graphURL = entry.graph_url;
@@ -314,6 +283,94 @@ document.addEventListener('DOMContentLoaded', () => {
       data2DArray.push(rowArray); //store all fetched data from table_questions into local 2d array data2DArray
     }
   }
+
+  /**
+   *  Display next prestudy question
+   */
+  function displayNextPrestudyQuestion() {
+    prestudyQuestionElement.innerHTML = currentQuestion.value =
+    userQuestions[currentPrestudyQuestionIndex][0];
+    prestudyChart.innerHTML = "";
   
+    var imageElement = document.createElement("img");
+    if (userQuestions[currentPrestudyQuestionIndex][1] != null) {
+      imageElement.src =
+        "img/prestudy-img/" + userQuestions[currentPrestudyQuestionIndex][1];
+      imageElement.alt = "prestudy Chart";
+      imageElement.style.width = "auto";
+      imageElement.style.height = "400px";
+      prestudyChart.appendChild(imageElement);
+    }
+
+    currentPrestudyQuestionIndex++;
+  }
+
+  /**
+   * Display next Question in question array
+   */
+  function displayNextQuestion() {
+    const currentRow = data2DArray[currentQuestionIndex];
+
+    // Assign value to the question text
+    questionElement.textContent = currentQuestion.value = `${currentQuestionIndex + 1}. ${currentRow[0]}`;
+    optionsElement.innerHTML = "";
+    chartPlaceholder.innerHTML = "";
+
+    currentCorrectAnswer = currentRow[4];
+
+    // Create iframe element using graphURL and URLParams
+    const graphURL = currentRow[1];
+    const iframeElement = document.createElement("iframe");
+    iframeElement.src = `${graphURL}`;
+    iframeElement.width = "100%";
+    iframeElement.height = "600px";
+    iframeElement.style.border = "none";
+    
+    const graphId = currentRow[7]
+
+    displayGraph(graphId)
+    
+    const options = currentRow[3]; 
+
+    options.forEach((option, index) => {
+      const label = document.createElement("label");
+      const input = document.createElement("input");
+      input.type = "radio";
+      input.name = "answer";
+      input.value = option;
+      label.appendChild(input);
+      label.appendChild(document.createTextNode(option));
+      optionsElement.appendChild(label);
+    });
+    
+    currentQuestionIndex++;
+  }
+
+  /**
+   * Displays appropriate graph based on graphId
+   */
+  function displayGraph(graphId) {
+    if(graphId === 1){
+      displayMyEquityGapsMajorGaps(chartPlaceholder);
+    } 
+    else if(graphId === 2){
+      displayStudentProgressUnits(chartPlaceholder);
+    }
+    else {
+      chartPlaceholder.appendChild(iframeElement);
+    }
+  }
+    
+  /**
+   * Reloads current graphs
+   */
+  function reloadGraph() {
+    while (chartPlaceholder.firstChild) {
+      chartPlaceholder.removeChild(chartPlaceholder.lastChild);
+    }
+
+    const currentRow = data2DArray[currentQuestionIndex];
+    displayGraph(currentRow[7])
+  }
 
 });
