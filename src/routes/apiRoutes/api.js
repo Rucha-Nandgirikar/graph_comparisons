4bfaf0cc3f9c17a776cf88a805eaaf5813ab395f
BradJKim <bradjkim03@gmail.com>
2024-10-21 20:49:31 -0700

    sequelize integrations

--- src/routes/apiRoutes/api.js
+++ src/routes/apiRoutes/api.js
@@ -1,18 +1,17 @@
 const express = require('express');
-
 const router = express.Router();
-const standardAndNonStandardGraphsController = require('../../controllers/standardAndNonStandardGraphComparisonController.js');
+const controller = require('./controllers/standardAndNonStandardGraphComparisonController');
 
-// Define the route for /claim-user-id
-router.get('/claim-user-id', standardAndNonStandardGraphsController.claimUserId);
-router.get('/test-questions', standardAndNonStandardGraphsController.getAllTestQuestions);
-router.get('/get-current-user-id',standardAndNonStandardGraphsController.getCurrentUserId);
+// User routes
+router.post('/users', controller.createUser);
+router.get('/users/:id', controller.getUserById);
 
-// POST Pre Study Response
-router.post('/submit-prestudy-response',standardAndNonStandardGraphsController.postPreStudyResponses);
-router.post('/submit-user-interaction',standardAndNonStandardGraphsController.postUserInteraction);
+// Question routes
+router.get('/questions', controller.getAllQuestions);
 
-// router.post('/insert-user-age',standardAndNonStandardGraphsController.postUserAge);
-
+// Response routes
+router.post('/responses/main', controller.addMainStudyResponse);
+router.post('/responses/pre', controller.addPreStudyResponse);
+router.post('/interactions', controller.addUserInteraction);
 
 module.exports = router;
